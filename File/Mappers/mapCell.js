e22d5c12e32789e08838762c7f230d3610702b59
ujorgeleite <jorgefernandodamasio@gmail.com>
2021-01-28 15:59:56 -0300

    fix(validation): add some validations in map Cell

--- File/Mappers/mapCell.js
+++ File/Mappers/mapCell.js
@@ -2,9 +2,17 @@ const isSimpleValue = (value) => {
   return !value.hyperlink && value.toString().length > 0;
 };
 
+const isDate = (value) => {
+  return value instanceof Date;
+};
+
 const mapCell = ({ value }) => {
   if (isSimpleValue(value)) {
-    return value;
+    if (isDate(value)) {
+      return value;
+    } else {
+      return value.toString().replace(" ", "");
+    }
   } else {
     return !!value.text ? value.text : "";
   }
